2ae55e6515501d86de6ab4a5ee5542fbdf821a93
davin <copperbeard@protonamail.com>
2020-09-10 10:58:53 +0100

    Completion of Katas
    Backspaces in strings  & Basic03 string and number calculation

---
+++ CodeWars/6kyu/Backspaces in string.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeWars._6kyu
+{
+    /**
+     * Assume "#" is like a backspace in string. 
+     * This means that string "a#bc#d" actually is "bd"
+     * 
+     * Your task is to process a string with "#" symbols.
+     * 
+     * "abc#d##c"      ==>  "ac"
+     * "abc##d######"  ==>  ""
+     * "#######"       ==>  ""
+     * ""              ==>  ""
+     * 
+     * */
+    public class Backspaces_in_string
+    {
+        internal void Solve()
+        {
+            string input = "abc#d##c";
+            // string result = "abc####d##c#"
+            var output = ParseString(input);
+
+            Console.WriteLine(output);
+        }
+        
+        public string ParseString(string input)
+        {
+           StringBuilder outputString = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char comparisonChar = input[i];
+                //    outputString.Append(input[i]);
+                if (comparisonChar != '#')
+                {
+                    outputString.Append(comparisonChar);
+                }
+                else if(outputString.Length > 0)
+                {
+                    //TODO: check for the length of the output string.
+                    outputString.Remove(outputString.Length-1, 1);
+                  
+                }
+                
+            }
+ return outputString.ToString();
+        }
+    }
+}
---
+++ CodeWars/6kyu/Basics03StringsNumbersAndCalculation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeWars._6kyu
+{
+    public class Basics03StringsNumbersAndCalculation
+    {
+        public void Solve()
+        {
+            string input = "fsdfsd235???34.4554s4234df-sdfgf2g3h4j442";
+            string result = ParseString(input);
+            Console.WriteLine(result);
+        }
+        string ParseString(string input)
+        {
+            string symbol = input.First(x =>
+            x.Equals('-') ||
+            x.Equals('*') ||
+            x.Equals('+') ||
+            x.Equals('/')
+            ).ToString();
+            var splitString = input.Split(symbol);
+
+            var first = splitString[0]
+                .Where(x => char.IsDigit(x) || x.Equals('.'))
+                .Aggregate("", (accumulator, x) => accumulator += x);
+            var second = splitString[1]
+                .Where(x => char.IsDigit(x) || x.Equals('.'))
+                .Aggregate("", (accumulator, x) => accumulator += x);
+
+
+
+            var result = symbol switch
+            {
+                "*" => double.Parse(first) * double.Parse(second),
+                "/" => double.Parse(first) / double.Parse(second),
+                "-" => double.Parse(first) - double.Parse(second),
+                "+" => double.Parse(first) + double.Parse(second)                
+            };
+            var output = Math.Round(result).ToString();
+            return output;
+           
+        }
+    }
+}
